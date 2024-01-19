aaca32e9b9fde30903a9bb6fc8b008dd4121ea47
Karthik Sankaran <karthik892@users.noreply.github.com>
2024-01-19 10:07:27 -0500

    Fully implemented bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import our custom CSS
+import './assets/main.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 const app = createApp(App)
 
 app.use(router)
