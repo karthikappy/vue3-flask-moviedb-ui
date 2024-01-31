f4bed092ecc4b6bd74685dfa0ab2a160bb5611e8
Karthik Sankaran <karthik892@users.noreply.github.com>
2024-01-30 20:54:13 -0500

    Replaced reactive with ref

--- client/src/components/PosterGrid.vue
+++ client/src/components/PosterGrid.vue
@@ -1,26 +1,26 @@
 <script setup>
 const props = defineProps(["type", "target", "title"])
 import { onMounted } from 'vue';
-import { reactive } from 'vue'
+import { ref } from 'vue'
 import TVPosterCard from './TVPosterCard.vue';
 import MoviePosterCard from './MoviePosterCard.vue';
 
-const apiData = reactive({})
+const apiData = ref({})
 
 onMounted(() => {
     fetch('api/' + props.target)
         .then(response => response.json())
         .then(data => {
-            apiData.data = data;
+            apiData.value = data;
         })
 }) 
 </script>
 <template>
-    <div class="card" v-if="apiData.data">
+    <div class="card" v-if="apiData">
         <div class="card-body">
             <h5 class="card-title">{{ title }}</h5>
             <div class="row">
-                <div class="col-md-2" v-for="item in apiData.data.results" :key="item.id">
+                <div class="col-md-2" v-for="item in apiData.results" :key="item.id">
                     <TVPosterCard v-if="type=='tv'" :item="item"/>
                     <MoviePosterCard v-if="type=='movie'" :item="item"/>
                 </div>
