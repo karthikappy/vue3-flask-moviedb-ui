ec788de46de96f4c6fc42e3247b8f1d0badc1e7f
Karthik Sankaran <karthik892@users.noreply.github.com>
2024-01-30 20:52:41 -0500

    Replaced reactive with ref

--- client/src/views/MovieView.vue
+++ client/src/views/MovieView.vue
@@ -1,35 +1,35 @@
 <script setup>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 import { onMounted } from 'vue';
 import { useRoute } from 'vue-router'
 
-const movieInfo = reactive({})
+const movieInfo = ref({})
 const route = useRoute()
 const id = route.params.id
 onMounted(() => {
     fetch('api/movie/details/' + id)
         .then(response => response.json())
         .then(data => {
-            movieInfo.data = data;
+            movieInfo.value = data;
         })
 }) 
 </script>
 <template>
     <main>
-        <div class="card" v-if="movieInfo.data">
-            <img :src="'http://image.tmdb.org/t/p/w780' + movieInfo.data.backdrop_path" class="card-img backdrop-img" alt="...">
+        <div class="card" v-if="movieInfo">
+            <img :src="'http://image.tmdb.org/t/p/w780' + movieInfo.backdrop_path" class="card-img backdrop-img" alt="...">
             <div class="card-img-overlay">
                 <div class="row">
-                    <div class="col-md-4"><img :src="'http://image.tmdb.org/t/p/w342' + movieInfo.data.poster_path" alt="" width="342"><img src="" alt="">
+                    <div class="col-md-4"><img :src="'http://image.tmdb.org/t/p/w342' + movieInfo.poster_path" alt="" width="342"><img src="" alt="">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h1 class="card-title">
-                                {{ movieInfo.data.title }}
-                                <span v-if="movieInfo.data.title != movieInfo.data.original_title">({{ movieInfo.data.original_title }})</span>
+                                {{ movieInfo.title }}
+                                <span v-if="movieInfo.title != movieInfo.original_title">({{ movieInfo.original_title }})</span>
                             </h1>
-                            <p class="card-text">{{ movieInfo.data.tagline }}</p>
-                            <p class="card-text">{{ movieInfo.data.overview }}</p>
+                            <p class="card-text">{{ movieInfo.tagline }}</p>
+                            <p class="card-text">{{ movieInfo.overview }}</p>
                         </div>
                     </div>
                 </div>
