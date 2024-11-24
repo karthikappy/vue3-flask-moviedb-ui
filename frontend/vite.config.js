d989a9e0f118458ac0f27ba085bccd4ec0e908d9
Karthik Sankaran <karthik892@users.noreply.github.com>
2024-11-24 14:01:21 -0500

    Updated vue and added DevTools plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
     }
   },
   plugins: [
-    vue(),
+    vue(),,
+    vueDevTools(),
   ],
   resolve: {
     alias: {
