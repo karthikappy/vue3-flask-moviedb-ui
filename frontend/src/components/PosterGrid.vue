49c56d21439266fd20c6dc3cada6c3ff6e161fd9
Karthik Sankaran <karthik892@users.noreply.github.com>
2025-06-20 14:59:35 -0400

    Added a carousel

--- frontend/src/components/PosterGrid.vue
+++ frontend/src/components/PosterGrid.vue
@@ -19,6 +19,24 @@ onMounted(() => {
     <div class="card" v-if="apiData">
         <div class="card-body">
             <h5 class="card-title">{{ title }}</h5>
+            <v-row>
+                <v-carousel hide-delimiters>
+                    <v-carousel-item 
+                        v-for="item in apiData.results" :key="item.id"
+                        :src="'http://image.tmdb.org/t/p/w780' + item.backdrop_path"
+                        cover
+                    >
+                        <v-card style="position: absolute;bottom:0px;left:0px;width:100%;background-color:rgba(0, 0, 0, 0.5);">
+                            <v-card-title><h3>{{ item.title || item.name }}</h3></v-card-title>
+                            <v-card-text><p>{{ item.overview }}</p></v-card-text>
+                        </v-card>
+                        <div class="text-center">
+                            
+                            
+                        </div>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-row>
             <div class="row">
                 <div class="col-md-2" v-for="item in apiData.results" :key="item.id">
                     <TVPosterCard v-if="type=='tv'" :item="item"/>
