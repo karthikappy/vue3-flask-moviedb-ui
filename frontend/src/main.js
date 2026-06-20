79bb5c985e6059c4ef4577a50c5fd4dd4edfa130
Karthik Sankaran <karthik892@users.noreply.github.com>
2026-06-19 20:33:49 -0400

    Migrate frontend store from Vuex to Pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,15 @@
 import './assets/main.scss'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 
 import App from './App.vue'
 import router from './router'
 import vuetify from './vuetify'
 
-const store = createStore({
-    state () {
-      return {
-        count: 0
-      }
-    },
-    mutations: {
-      increment (state) {
-        state.count++
-      }
-    }
-  })
-
 const app = createApp(App)
 
 app.use(router)
-app.use(store)
+app.use(createPinia())
 app.use(vuetify)
 app.mount('#app')
